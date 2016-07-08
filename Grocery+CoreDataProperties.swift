62f2ae054e648abb16b1718fb1e5b0d4cf7e0e51
Daniel J Janiak <danieljjaniak@Daniels-Air.attlocal.net>
2016-07-08 12:25:04 -0700

    Create a subclass of the NSManagedObject

---
+++ Grocery+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Grocery+CoreDataProperties.swift
+//  Shopping List
+//
+//  Created by Daniel J Janiak on 7/8/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Grocery {
+
+    @NSManaged var item: String?
+
+}
---
+++ Grocery.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery.swift
+//  Shopping List
+//
+//  Created by Daniel J Janiak on 7/8/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Grocery: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
